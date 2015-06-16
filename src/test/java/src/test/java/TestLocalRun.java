5cd35ce9b6eb4030588bc2c06d8d4da8d242dc8b
Amanda M <amarrione@gmail.com>
2015-06-16 09:00:02 -0400

    first commit

---
+++ src/test/java/src/test/java/TestLocalRun.java
@@ -0,0 +1,38 @@
+package src.test.java;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+ 
+public class TestLocalRun {
+     private WebDriver driver;
+     @SuppressWarnings("unused")
+	private WebDriverWait wait;
+    
+    
+      @Before
+      public void setUp() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.get("http://www.google.com");
+      }
+      @After
+      public void tearDown() throws IOException {
+        driver.quit();
+      } 
+ 
+      @Test
+      public void pageTitleAfterSearchShouldBeginWithDrupal() throws IOException {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Drupal!");
+        searchField.submit();
+      }
+    
+}
