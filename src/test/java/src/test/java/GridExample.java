ebf042ebf2281c0eebc854b2ab96a6bfd8bccd24
Amanda M <amarrione@gmail.com>
2015-06-17 14:50:47 -0400

    commiting some things

---
+++ src/test/java/src/test/java/GridExample.java
@@ -0,0 +1,32 @@
+package src.test.java;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class GridExample {
+	
+	@Test
+	public void testTime() throws MalformedURLException {
+		new DesiredCapabilities();
+		DesiredCapabilities dr = DesiredCapabilities.firefox();
+		
+		
+		
+		RemoteWebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dr);
+		
+		driver.navigate().to("http://gmail.com");
+		
+		driver.findElement(By.xpath("//input[@id='Email']")) .sendKeys("username");
+		
+		driver.findElement(By.xpath("//input[@id='Passwd']")) .sendKeys("password");
+		
+		driver.close();
+
+	}
+
+}
